4f42fe78fbb9d1241872fb3d21f74492ce2594a7
foresitegroup <mark@foresitegrp.com>
2019-03-11 09:31:48 -0500

    Add survey results page

---
+++ survey-results.php
@@ -0,0 +1,82 @@
+<?php
+$PageTitle = "Survey Results";
+include "header.php";
+?>
+
+<div class="sub-header tb-header mb" style="background: url(images/sub-header-uam.jpg);">
+  <article class="vert-center">
+    IAM INDIVIDUAL<br>SURVEY RESULTS
+  </article>
+
+  <?php include "trialbox-iam.php"; ?>
+</div>
+
+<article id="main">
+  The founders of QUANT IX SOFTWARE Inc., the developer of the Investment Account Manager software programs, have been registered and regulated investment advisors since the 1980s.  As such, we instinctively developed (since 1985) an investment management software program incorporating the fiduciary rule principal in its design and features.  This important distinction is yet another reason why our customers know we always put their welfare first.  And as you'll gather from the results listed below from our recent survey (January 2019), we've been successful in building long-term relationships with our users that have found IAM3 Individual our best release yet for successfully managing their investment portfolios.<br>
+  <br>
+  <br>
+
+  The chart below illustrates user replies to "<strong>How many years have you been using IAM3 Individual or a predecessor program?</strong>"  We are thankful for our long-time users (nearly 55% > than 10+ years), with some users selecting the 'other - write in' option to indicate they've been with us for over 30+ years!<br>
+  <br>
+
+  <img src="images/survey-2019-1.png" alt=""><br>
+  <br>
+  <br>
+
+  The chart below illustrates user replies to "<strong>How many investment portfolio accounts do you manage in IAM3 Individual?</strong>"  It seems clear most of our users are using IAM3 to track multiple portfolios, including > 12% of our users tracking in excess of 11+ portfolios.  IAM3 is an ideal tool for unified account management.<br>
+  <br>
+
+  <img src="images/survey-2019-2.png" alt=""><br>
+  <br>
+  <br>
+
+  The chart below illustrates user replies to "<strong>What type of investments are you tracking in IAM3 Individual?</strong>"  The majority of our users are following a balanced allocation across multiple assets types.  Users selecting the 'other - write in' option also indicated they own a variety of fixed income investment types as well as options.<br>
+  <br>
+
+  <img src="images/survey-2019-3.png" alt=""><br>
+  <br>
+  <br>
+
+  The chart below illustrates user replies to "<strong>What is the cumulative value of the assets under management you are tracking within IAM3 Individual?</strong>"  It's rewarding to know our users are well represented across a wide spectrum of total portfolio(s) values, and that IAM3 is proving to be a valuable tool in helping our users manage their wealth for long-term success!<br>
+  <br>
+
+  <img src="images/survey-2019-4.png" alt=""><br>
+  <br>
+  <br>
+
+  The chart below illustrates user replies to "<strong>Which features do you find most helpful in managing your investment account(s)?</strong>"  Accurate transaction accounting, detailed reporting, and managing multiple portfolios are most helpful to our users.  Also included are the comprehensive tax planning tools and allocation rebalancing features.  As our users have learned, IAM3 Individual offers a comprehensive set of features to better manage their investment account(s).<br>
+  <br>
+
+  <img src="images/survey-2019-5.png" alt=""><br>
+  <br>
+  <br>
+
+  The chart below illustrates user replies to "<strong>How many stars would you award IAM3 Individual (5 highest, 1 lowest)?</strong>" Since 1985, we have been providing award-winning, affordable investment software, focused on providing comprehensive, effective and easy to use features.   And for those times when users do have questions on using IAM3 Individual, we're glad to know we're providing timely, helpful technical support!<br>
+  <br>
+
+  <img src="images/survey-2019-6.png" alt=""><br>
+  <br>
+  <br>
+
+  The list below is just a sampling from user replies to "<strong>What is, in your opinion, the greatest strength in using Investment Account Manager?</strong>"  We received a lengthy list of replies, with many accolades for the functionality, accuracy, longevity, and our outstanding technical support.  We're confident you too would form similar opinions if you started using IAM3 Individual to help you track your personal investments!<br>
+  <br>
+
+  <ul>
+    <li>Putting everything in one place, but with transaction detail so that one tool serves my global strategy and detail tactical monitoring and decision making.</li>
+    <li>Tech support; reporting; ease of data entry and downloading.</li>
+    <li>Account consolidation or rollup to see complete picture of all accounts for an asset allocation strategy.</li>
+    <li>Ability to download brokerage account activity and view that activity easily.</li>
+    <li>The ability to track multiple purchases of a stock (such as a dividend reinvestment plan), and to be able to accurately report capital gains within seconds.</li>
+    <li>Calculation of ROI for a portfolio or individual holding for any time period requested!!</li>
+    <li>I can segregate different accounts (IRA, Roth, Stocks, mutual fund, & family members).  This helps me to keep track of how each is performing without piles of paperwork.</li>
+    <li>Excellent tech support.</li>
+    <li>The only individual software that provides an accurate rate of internal return.</li>
+    <li>IAM simply works and works well. Able to handle complex transactions accurately. When a problem arises, tech support quickly, and I mean quickly, responds.</li>
+    <li>I like tracking IRR for individual and total portfolios and quick and concise looks of all my assets. The reports are helpful. I like being able to drill down on the mutual funds to see the cumulative allocation of all assets. The allocation reports help me to see if I am properly allocated.</li>
+    <li>Easy to use.</li>
+    <li>It works! On the rare (maybe 3 times in 15 years) times I needed help, it was right there with courteous service.</li>
+    <li>Reliable, detailed, record keeping of all transactions</li>
+  </ul>
+</article>
+
+<?php include "footer.php"; ?>
