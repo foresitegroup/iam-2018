122ae20fdd743839b239b17611aa809a485a5439
foresitegroup <mark@foresitegrp.com>
2019-07-03 13:42:28 -0500

    Update how to pages

---
+++ how-to-downloading-your-yahoo-finance-portfolios.php
@@ -0,0 +1,97 @@
+
+<?php
+$PageTitle = "Downloading Your Yahoo Finance Portfolios";
+include "header.php";
+?>
+
+<div class="red mb">
+  <article>
+    <h1>DOWNLOADING YOUR YAHOO FINANCE PORTFOLIOS</h1>
+  </article>
+</div>
+
+<article class="how-to">
+  Having the ability to manage your portfolio holdings within Investment Account Manager will provide many important tools and reports not available in Yahoo! Finance. IAM allows:<br>
+  <br>
+
+  <ul>
+    <li>Centralized Secure Portfolio Management -- Desktop Application -- totally private</li>
+    <li>Multiple Portfolio Management and Collective Reporting</li>
+    <li>Detailed, Informative and Customizable Reporting</li>
+    <li>Portfolio Allocation and Rebalancing Analysis</li>
+    <li>Mutual Fund Composition Analysis -- Asset Type, Sector</li>
+    <li>Portfolio Performance with Benchmark Comparisons</li>
+    <li>Fundamental Ratio Analysis</li>
+    <li>Financial Institution Downloads</li>
+    <li>Automatic Security Price Updating</li>
+    <li>Mutual Fund Composition Analysis</li>
+    <li>Create Historical Pricing Databases</li>
+  </ul>
+  <br>
+
+  Since 1985 Investment Account Manager has provided investors with detailed portfolio management tools and professional reporting.<br>
+  <br>
+
+  <strong>Importing from Yahoo! Finance</strong><br>
+  <ol>
+    <li>
+      Go to <a href="https://finance.yahoo.com">https://finance.yahoo.com</a> and then select My Portfolio. Select the portfolio you wish to export to IAM to open within Yahoo!.<br>
+      <img src="images/how-to-downloading-your-yahoo-finance-portfolios1.jpg" alt=""><br>
+      <br>
+    </li>
+
+    <li>
+      Next click on Export. This will create a file called "quotes.csv" which will be saved to your PC's download folder.<br>
+      <img src="images/how-to-downloading-your-yahoo-finance-portfolios2.jpg" alt=""><br>
+      <br>
+    </li>
+  </ol>
+  <br>
+
+  <strong>Creating the portfolio in Investment Account Manager</strong><br>
+  <ol>
+    <li>
+      Start IAM and selected File Menu | Import | Yahoo Portfolio Data.<br>
+      <img src="images/how-to-downloading-your-yahoo-finance-portfolios3.jpg" alt=""><br>
+      <br>
+    </li>
+
+    <li>
+      Now give the portfolio a name within Investment Account Manager, and then click on Next.<br>
+      <img src="images/how-to-downloading-your-yahoo-finance-portfolios4.jpg" alt=""><br>
+      <br>
+    </li>
+
+    <li>
+      Select the location of the file downloaded from Yahoo!, then the file, and then the Select button and then click on import.<br>
+      <img src="images/how-to-downloading-your-yahoo-finance-portfolios5.jpg" alt=""><br>
+      <img src="images/how-to-downloading-your-yahoo-finance-portfolios6.jpg" alt=""><br>
+      <br>
+    </li>
+
+    <li>
+      Check the following screens closely for any editing which may be needed.<br>
+      <img src="images/how-to-downloading-your-yahoo-finance-portfolios7.jpg" alt=""><br>
+      <img src="images/how-to-downloading-your-yahoo-finance-portfolios8.jpg" alt=""><br>
+      <br>
+    </li>
+
+    <li>
+      Review IAM's import audit report.<br>
+      <img src="images/how-to-downloading-your-yahoo-finance-portfolios9.jpg" alt=""><br>
+      <br>
+    </li>
+  </ol>
+  <br>
+
+  Your portfolio is now part of Investment Account Manager, as shown on IAM's Home Page.<br>
+  <img src="images/how-to-downloading-your-yahoo-finance-portfolios10.jpg" alt="">
+</article>
+
+<div class="darkgray whitetext footer bt">
+  <article>
+    <h2>NEED HELP? TRY OUR <a href="support.php">SUPPORT</a> PAGE</h2>
+  </article>
+</div>
+
+<?php include "footer.php"; ?>
---
+++ how-to-uploading-your-portfolio-to-yahoo-finance.php
@@ -0,0 +1,82 @@
+
+<?php
+$PageTitle = "Uploading Your Portfolio to Yahoo Finance";
+include "header.php";
+?>
+
+<div class="red mb">
+  <article>
+    <h1>UPLOADING YOUR PORTFOLIO TO YAHOO FINANCE</h1>
+  </article>
+</div>
+
+<article class="how-to">
+  Having the ability to monitor your portfolio holdings online is easy to accomplish with IAM and Yahoo Finance. Simply follow these steps.<br>
+  <br>
+
+  Users will need to decide: Do I want my actual share values (lot by lot) uploaded, or simply the security names without actual values.<br>
+  <br>
+
+  Both methods provide meaningful results available for review by cell phone, tablet, etc. after installing the Yahoo! Finance free application on your device.<br>
+  <br>
+
+  <strong>To Import your Investment Account Manager portfolio, simply follow these steps:</strong><br>
+  <ol>
+    <li>
+      Open IAM and select the portfolio from the portfolios list.<br>
+      <img src="images/how-to-uploading-your-portfolio-to-yahoo-finance1.jpg" alt=""><br>
+      <br>
+    </li>
+
+    <li>
+      Select File Menu | Export | Yahoo Portfolio Data.<br>
+      <img src="images/how-to-uploading-your-portfolio-to-yahoo-finance2.jpg" alt=""><br>
+      <br>
+    </li>
+
+    <li>
+      Select your preference: Tax lots (actual portfolio) or One Share of Each Asset.<br>
+      <img src="images/how-to-uploading-your-portfolio-to-yahoo-finance3.jpg" alt=""><br>
+      <br>
+    </li>
+
+    <li>
+      Investment Account Manager will create and save the file to use with Yahoo! Finance.  Note the location of the file so you can find it.<br>
+      <img src="images/how-to-uploading-your-portfolio-to-yahoo-finance4.jpg" alt=""><br>
+      <br>
+    </li>
+
+    <li>When complete, close the open windows and minimize Investment Account Manager.</li>
+  </ol>
+  <br>
+  <br>
+
+  <strong>Steps to complete in Yahoo</strong><br>
+  <ol>
+    <li>Log-into Yahoo (<a href="http://www.yahoo.com">http://www.yahoo.com</a>) or download and install the Smartphone app to your device.<br><br></li>
+    <li>Either sign-in or sign-up for a new account.<br><br></li>
+    <li>After logging into your account, select Finance from the left panel on Yahoo's Home Page.  This will take you to Yahoo Finance home page (<a href="http://finance.yahoo.com">http://finance.yahoo.com</a>).<br><br></li>
+    <li>Click on "My Portfolio" in the Menu Bar and then Create Portfolio.<br><br></li>
+
+    <li>Look for "Import". Click on "Import".<br><br></li>
+
+    <li>
+      In the Import window click on "Choose File". Find the file, then double-click on it to select it for import to Yahoo!, and Submit button. After the page refreshes, you will have uploaded your portfolio to Yahoo!<br>
+      <img src="images/how-to-uploading-your-portfolio-to-yahoo-finance4.jpg" alt=""><br>
+      <br>
+    </li>
+
+    <li>The final step is to rename your portfolio on Yahoo!'s website: Click on "Settings" then Rename.</li>
+  </ol>
+  <br>
+
+  <strong>That's it... you now can monitor your portfolio from anywhere in the world!</strong>
+</article>
+
+<div class="darkgray whitetext footer bt">
+  <article>
+    <h2>NEED HELP? TRY OUR <a href="support.php">SUPPORT</a> PAGE</h2>
+  </article>
+</div>
+
+<?php include "footer.php"; ?>
