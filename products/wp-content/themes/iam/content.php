9eaab6cc169725147c18000b61e516c69e9ef4d1
foresitegroup <mark@foresitegrp.com>
2019-09-03 11:19:20 -0500

    Fix date display on single blog entry

--- products/wp-content/themes/iam/content.php
+++ products/wp-content/themes/iam/content.php
@@ -59,7 +59,24 @@
 	<footer class="entry-footer">
 		<?php
 		if (is_single()) {
-			twentyfifteen_entry_meta();
+			// twentyfifteen_entry_meta();
+			echo '<span class="posted-on"><span>Posted on </span><a href="'.get_permalink().'">'.get_the_date('F j, Y')."</a></span>\n";
+
+			if ('post' == get_post_type()) {
+        if (is_singular() || is_multi_author()) {
+          printf( '<span class="byline"><span class="author vcard"><span class="screen-reader-text">%1$s </span><a class="url fn n" href="%2$s">%3$s</a></span></span>', _x('Author', 'Used before post author name.', 'twentyfifteen'), esc_url(get_author_posts_url(get_the_author_meta('ID'))), get_the_author());
+        }
+
+        $categories_list = get_the_category_list(_x(', ', 'Used between list items, there is a space after the comma.', 'twentyfifteen')); 
+        if ($categories_list && twentyfifteen_categorized_blog()) { 
+          printf('<span class="cat-links"><span class="screen-reader-text">%1$s </span>%2$s</span>', _x( 'Categories', 'Used before category names.', 'twentyfifteen'), $categories_list); 
+        }
+ 
+        $tags_list = get_the_tag_list('', _x(', ', 'Used between list items, there is a space after the comma.', 'twentyfifteen')); 
+        if ($tags_list) { 
+          printf('<span class="tags-links"><span class="screen-reader-text">%1$s </span>%2$s</span>', _x('Tags', 'Used before tag names.', 'twentyfifteen'), $tags_list); 
+        }
+      }
 		} else {
 			echo '<span class="posted-on"><span>Posted on </span><a href="'.get_permalink().'">'.get_the_date('F j, Y')."</a></span>\n";
 		}
