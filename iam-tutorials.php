05653089d02a58eb2f54cf87ca0585b98f55e82a
foresitegroup <mark@foresitegrp.com>
2020-02-13 12:31:33 -0600

    Replace video

--- iam-tutorials.php
+++ iam-tutorials.php
@@ -145,7 +145,7 @@ include "header.php";
   <h2 class="red" id="portfolio-setup-wizard">Portfolio Setup Wizard</h2>
   <div class="vid-wrap">
     <div class="vid">
-      <a href="https://www.youtube.com/watch?v=oEaYCC5BKZU&rel=0&showinfo=0&vq=hd1080" class="swipebox-video" rel="oEaYCC5BKZU"></a>
+      <a href="https://www.youtube.com/watch?v=R6qnS1a2yzI&rel=0&showinfo=0&vq=hd1080" class="swipebox-video" rel="R6qnS1a2yzI"></a>
 
       <div class="video-text">
         <h4>Download Current Holdings From Your Financial Institution</h4>
