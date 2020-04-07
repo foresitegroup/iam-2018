af26eb5cf524c05ea42e62642ce7a411d1288933
marklippert <lippert@gmail.com>
2020-04-07 11:01:44 -0500

    Update surbey page for 2020

--- survey-results.php
+++ survey-results.php
@@ -12,70 +12,83 @@ include "header.php";
 </div>
 
 <article id="main">
-  The founders of QUANT IX SOFTWARE Inc., the developer of the Investment Account Manager software programs, have been registered and regulated investment advisors since the 1980s.  As such, we instinctively developed (since 1985) an investment management software program incorporating the fiduciary rule principal in its design and features.  This important distinction is yet another reason why our customers know we always put their welfare first.  And as you'll gather from the results listed below from our recent survey (January 2019), we've been successful in building long-term relationships with our users that have found IAM3 Individual our best release yet for successfully managing their investment portfolios.<br>
+  The founders of QUANT IX SOFTWARE Inc., the developer of the Investment Account Manager software programs, have been registered and regulated investment advisors since the 1980s.  As such, we instinctively developed (since 1985) an investment management software program incorporating the fiduciary rule principal in its design and features.  This important distinction is yet another reason why our customers know we always put their welfare first.  And as you'll gather from the results listed below from our recent survey (March 2020), we've been successful in building long-term relationships with our users that have found IAM3 Individual our best release yet for successfully managing their investment portfolios.<br>
   <br>
   <br>
 
-  The chart below illustrates user replies to "<strong>How many years have you been using IAM3 Individual or a predecessor program?</strong>"  We are thankful for our long-time users (nearly 55% > than 10+ years), with some users selecting the 'other - write in' option to indicate they've been with us for over 30+ years!<br>
+  The chart below illustrates user replies to "How many years have you been using IAM3 Individual or a predecessor program?"  We are thankful for our long-time users (nearly 54% > than 10+ years), with some users selecting the 'other - write in' option to indicate they've been with us for over 30+ years!<br>
   <br>
 
-  <img src="images/survey-2019-1.png" alt=""><br>
-  <br>
-  <br>
-
-  The chart below illustrates user replies to "<strong>How many investment portfolio accounts do you manage in IAM3 Individual?</strong>"  It seems clear most of our users are using IAM3 to track multiple portfolios, including > 12% of our users tracking in excess of 11+ portfolios.  IAM3 is an ideal tool for unified account management.<br>
-  <br>
-
-  <img src="images/survey-2019-2.png" alt=""><br>
+  <img src="images/survey-2020-1.jpg" alt=""><br>
   <br>
   <br>
 
-  The chart below illustrates user replies to "<strong>What type of investments are you tracking in IAM3 Individual?</strong>"  The majority of our users are following a balanced allocation across multiple assets types.  Users selecting the 'other - write in' option also indicated they own a variety of fixed income investment types as well as options.<br>
+  The chart below illustrates user replies to "How many investment portfolio accounts do you manage in IAM3 Individual?"  It seems clear most of our users are using IAM3 to track multiple portfolios, including > 12% of our users tracking in excess of 11+ portfolios.  IAM3 is an ideal tool for unified account management. <br>
   <br>
 
-  <img src="images/survey-2019-3.png" alt=""><br>
-  <br>
-  <br>
-
-  The chart below illustrates user replies to "<strong>What is the cumulative value of the assets under management you are tracking within IAM3 Individual?</strong>"  It's rewarding to know our users are well represented across a wide spectrum of total portfolio(s) values, and that IAM3 is proving to be a valuable tool in helping our users manage their wealth for long-term success!<br>
-  <br>
-
-  <img src="images/survey-2019-4.png" alt=""><br>
+  <img src="images/survey-2020-2.jpg" alt=""><br>
   <br>
   <br>
 
-  The chart below illustrates user replies to "<strong>Which features do you find most helpful in managing your investment account(s)?</strong>"  Accurate transaction accounting, detailed reporting, and managing multiple portfolios are most helpful to our users.  Also included are the comprehensive tax planning tools and allocation rebalancing features.  As our users have learned, IAM3 Individual offers a comprehensive set of features to better manage their investment account(s).<br>
+  The chart below illustrates user replies to "What type of investments are you tracking in IAM3 Individual?"  The majority of our users are following a balanced allocation across multiple assets types.  Users selecting the 'other - write in' option also indicated they own a variety of fixed income investment types as well as options.<br>
   <br>
 
-  <img src="images/survey-2019-5.png" alt=""><br>
-  <br>
-  <br>
-
-  The chart below illustrates user replies to "<strong>How many stars would you award IAM3 Individual (5 highest, 1 lowest)?</strong>" Since 1985, we have been providing award-winning, affordable investment software, focused on providing comprehensive, effective and easy to use features.   And for those times when users do have questions on using IAM3 Individual, we're glad to know we're providing timely, helpful technical support!<br>
-  <br>
-
-  <img src="images/survey-2019-6.png" alt=""><br>
+  <img src="images/survey-2020-3.jpg" alt=""><br>
   <br>
   <br>
 
-  The list below is just a sampling from user replies to "<strong>What is, in your opinion, the greatest strength in using Investment Account Manager?</strong>"  We received a lengthy list of replies, with many accolades for the functionality, accuracy, longevity, and our outstanding technical support.  We're confident you too would form similar opinions if you started using IAM3 Individual to help you track your personal investments!<br>
+  The chart below illustrates user replies to "What is the cumulative value of the assets under management you are tracking within IAM3 Individual?"  It's rewarding to know our users are well represented across a wide spectrum of total portfolio(s) values, and that IAM3 is proving to be a valuable tool in helping our users manage their wealth for long-term success!<br>
+  <br>
+
+  <img src="images/survey-2020-4.jpg" alt=""><br>
+  <br>
+  <br>
+
+  The chart below illustrates user replies to "Which features do you find most helpful in managing your investment account(s)?"  Accurate transaction accounting, detailed reporting, and managing multiple portfolios are most helpful to our users.  Also included are the comprehensive tax planning tools, allocation rebalancing features, and investment selection and analysis.  As our users have learned, IAM3 Individual offers a comprehensive set of features to better manage their investment account(s).<br>
+  <br>
+
+  <img src="images/survey-2020-5.jpg" alt=""><br>
+  <br>
+  <br>
+
+  The chart below illustrates user replies to "If you download your investment data, which financial institution(s) do you use?" Investment Account Manager supports automatic, secure downloads from many leading brokers, including these as shown below.<br>
+  <br>
+
+  <img src="images/survey-2020-6.jpg" alt=""><br>
+  <br>
+  <br>
+
+  The chart below illustrates user replies to "How many stars would you award IAM3 Individual (5 highest, 1 lowest)?" Since 1985, we have been providing award-winning, affordable investment software, focused on providing comprehensive, effective and easy to use features.   And for those times when users do have questions on using IAM3 Individual, we're glad to know we're providing timely, helpful technical support!<br>
+  <br>
+
+  <img src="images/survey-2020-7.jpg" alt=""><br>
+  <br>
+  <br>
+
+  The list below is just a sampling from user replies to "What is, in your opinion, the greatest strength in using Investment Account Manager?  We received a lengthy list of replies, with many accolades for the functionality, accuracy, longevity, and our outstanding technical support.  We're confident you too would form similar opinions if you started using IAM3 Individual to help you track your personal investments!<br>
   <br>
 
   <ul>
-    <li>Putting everything in one place, but with transaction detail so that one tool serves my global strategy and detail tactical monitoring and decision making.</li>
-    <li>Tech support; reporting; ease of data entry and downloading.</li>
-    <li>Account consolidation or rollup to see complete picture of all accounts for an asset allocation strategy.</li>
-    <li>Ability to download brokerage account activity and view that activity easily.</li>
-    <li>The ability to track multiple purchases of a stock (such as a dividend reinvestment plan), and to be able to accurately report capital gains within seconds.</li>
-    <li>Calculation of ROI for a portfolio or individual holding for any time period requested!!</li>
-    <li>I can segregate different accounts (IRA, Roth, Stocks, mutual fund, & family members).  This helps me to keep track of how each is performing without piles of paperwork.</li>
-    <li>Excellent tech support.</li>
-    <li>The only individual software that provides an accurate rate of internal return.</li>
-    <li>IAM simply works and works well. Able to handle complex transactions accurately. When a problem arises, tech support quickly, and I mean quickly, responds.</li>
-    <li>I like tracking IRR for individual and total portfolios and quick and concise looks of all my assets. The reports are helpful. I like being able to drill down on the mutual funds to see the cumulative allocation of all assets. The allocation reports help me to see if I am properly allocated.</li>
-    <li>Easy to use.</li>
-    <li>It works! On the rare (maybe 3 times in 15 years) times I needed help, it was right there with courteous service.</li>
-    <li>Reliable, detailed, record keeping of all transactions</li>
+    <li>There are many strengths.  The most important one for me is the accuracy of transactions and recordkeeping.  Close behind is the different forms of reporting that allow me to use that information for decision making and tax planning/preparation.</li>
+    <li>The best feature -- and why I use IAM -- is accurate reporting of portfolio balances and returns. Also, it is easy to combine accounts (e.g., all IRAs or all taxable accounts) for reporting balances and returns.</li>
+    <li>Being able to judge an individual stock's performance over a variety of time periods.</li>
+    <li>It is reliable, I've used IAM since the 3.5-inch floppy disk days.  Many years of data reliably stored and retrieved.</li>
+    <li>Reporting across (i.e., combining) portfolios. Calculating internal rates of return for variable time periods.</li>
+    <li>Quick, easy, accurate investment valuation and performance reporting.</li>
+    <li>Accuracy and functionality for the price. IAM also handles option transactions much better than other portfolio management tools I've used.</li>
+    <li>Accuracy of information and reliability of the system.  Thank you!! This is such a valuable aspect of IAM.</li>
+    <li>Ease of use and program flexibility once all initial data is loaded into the program.  It's a very powerful tool and I would never have continued investing without it.</li>
+    <li>Tracking assets and end of year tax information.  Allows comparison with what the investment agents have sent to you for tax reporting.  Also, if the IRS questions what you did you have backup data.</li>
+    <li>Ability to track realized gains/losses, interest income and dividend income throughout the year for tax planning (and to verify the accuracy of the monthly brokerage statements).</li>
+    <li>It is bulletproof.  For what I need it for, it is extremely reliable; you can trust the results.</li>
+    <li>Portfolio analysis, and analysis of combing several portfolios together.</li>
+    <li>Helps me stay organized in my investing and provides an easy method to look at all my investments in total, review total portfolio balance and performance.</li>
+    <li>Consolidated view of my portfolio and allocations that include mutual fund IRAs, self-managed brokerage account, advisor managed acct, and work plan accounts.</li>
+    <li>Reporting</li>
+    <li>The accuracy, comprehensiveness, and versatility.</li>
+    <li>Accuracy of downloaded activity from broker.</li>
+    <li>Ease of use and report generating.</li>
+    <li>Consolidate all your recordkeeping in one place and create reports that are very useful.</li>
   </ul>
 </article>
 
